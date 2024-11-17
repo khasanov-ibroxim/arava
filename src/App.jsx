6ff230b9c126e7588cf8fb373fbfd46c8e7748db
ibrohim74 <xibroxim@gmail.com>
2024-11-17 16:03:39 +0500

    firsts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, HashRouter, Route, Routes, useLocation} from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { SELLER_LAYOUT, USER_LAYOUT } from "./utils/const.jsx";
 import UserLayout from "./pages/userLayout/userLayout.jsx";
 import SellerLayout from "./pages/sellerLayout/sellerLayout.jsx";
@@ -7,26 +7,29 @@ import UserHome from "./pages/userPages/home/userHome.jsx";
 import SellerHome from "./pages/sellerPages/home/sellerHome.jsx";
 
 const AppContent = () => {
-    const role = "user";
+    const role = "user"; // Foydalanuvchi roli "user" yoki "seller" sifatida o'rnatiladi
     return (
-        <HashRouter>
-            <Route path="/:user_id/:language" element={role === "user" ? <UserHome/> : <SellerHome/>} />
+        <Routes>
+            <Route
+                path="/:user_id/:language"
+                element={role === "user" ? <UserHome /> : <SellerHome />}
+            />
             {role === "user" && (
                 <Route path={USER_LAYOUT + "*"} element={<UserLayout />} />
             )}
             {role === "seller" && (
                 <Route path={SELLER_LAYOUT + "*"} element={<SellerLayout />} />
             )}
-            {/*<Route path="*" element={<div>asdsadas</div>} />*/}
-        </HashRouter>
+            <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
     );
 };
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <AppContent />
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
