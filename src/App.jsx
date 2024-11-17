c84fee8193e6e2a63ec0a7bef4c74fbb4998f465
ibrohim74 <xibroxim@gmail.com>
2024-11-17 15:30:58 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { SELLER_LAYOUT, USER_LAYOUT } from "./utils/const.jsx";
 import UserLayout from "./pages/userLayout/userLayout.jsx";
 import SellerLayout from "./pages/sellerLayout/sellerLayout.jsx";
+import UserHome from "./pages/userPages/home/userHome.jsx";
+import SellerHome from "./pages/sellerPages/home/sellerHome.jsx";
 
 const AppContent = () => {
     const role = "user";
     return (
         <Routes>
+            <Route path="/:user_id/:language" element={role === "user" ? <UserHome/> : <SellerHome/>} />
             {role === "user" && (
                 <Route path={USER_LAYOUT + "*"} element={<UserLayout />} />
             )}
