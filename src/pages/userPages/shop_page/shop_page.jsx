97492c4dffef3edc98bf4d315f4cd92d049e469f
ibrohim74 <xibroxim@gmail.com>
2024-11-20 20:53:37 +0500

    firsts

--- src/pages/userPages/shop_page/shop_page.jsx
+++ src/pages/userPages/shop_page/shop_page.jsx
@@ -1,175 +1,86 @@
 import React, { useState, useRef } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css"; // Swiper uchun CSS
 import "./shop_page.css";
 
-import Banner from "../../../assets/img/image (1).png";
-import Banner2 from "../../../assets/img/Group 18.png";
-import Banner3 from "../../../assets/img/Group 19.svg";
-import LocalGroceryStoreRoundedIcon from "@mui/icons-material/LocalGroceryStoreRounded";
-
-
-const category = [
-    { id: 1, category_name: "Go'sht" },
-    { id: 2, category_name: "Tuxum" },
-    { id: 3, category_name: "Sabzavotlar" },
-    { id: 4, category_name: "Mevalar" },
-    { id: 5, category_name: "Dorixona" },
-];
-
-const product = [
-    {
-        id: 1,
-        img_url:Banner,
-        name: "Mol go'shti qovirga",
-        price: "72900",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
-        category_id: "1",
-    }, {
-        id: 1,
-        img_url:"https://yukber.uz/image/cache/catalog/product/YK1712/YK1712-600x600.jpg",
-        name: "Go'sht",
-        price: "72900",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
-        category_id: "1",
-    }, {
-        id: 1,
-        img_url:"https://yukber.uz/image/cache/catalog/product/YK1712/YK1712-600x600.jpg",
-        name: "Go'sht",
-        price: "72900",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
-        category_id: "1",
-    }, {
-        id: 1,
-        img_url:"https://yukber.uz/image/cache/catalog/product/YK1712/YK1712-600x600.jpg",
-        name: "Go'sht",
-        price: "72900",
-        info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.`,
-        category_id: "1",
-    },
-    {
-        id: 2,
-        img_url:"",
-        name: "Tuxum",
-        price: "25000",
-        info: `Yangi va sifatli tuxumlar.`,
-        category_id: "2",
-    },
-    {
-        id: 3,
-        img_url:"",
-        name: "Kartoshka",
-        price: "15000",
-        info: `Yangi sabzavot.`,
-        category_id: "3",
-    },
-    {
-        id: 4,
-        img_url:"",
-        name: "Olma",
-        price: "20000",
-        info: `Mevalar orasida mashhur.`,
-        category_id: "4",
-    },
-    {
-        id: 5,
-        img_url:"",
-        name: "Vitaminlar",
-        price: "50000",
-        info: `Dorixona mahsuloti.`,
-        category_id: "5",
-    },
-];
-
 const ShopPage = () => {
-    const [products] = useState(product);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const modalRef = useRef(null);
 
-    // Har bir kategoriya uchun ref yaratamiz
-    const categoryRefs = useRef([]);
+    // Swipe uchun o'zgaruvchilar
+    const [touchStart, setTouchStart] = useState(0);
+    const [touchEnd, setTouchEnd] = useState(0);
 
-    // Scroll qilish funksiyasi
-    const scrollToCategory = (id) => {
-        const targetRef = categoryRefs.current[id];
-        if (targetRef) {
-            targetRef.scrollIntoView({ behavior: "smooth", block: "start" });
+    const openModal = (product) => {
+        setSelectedProduct(product);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setSelectedProduct(null);
+    };
+
+    const handleTouchStart = (e) => {
+        setTouchStart(e.touches[0].clientY); // Boshlang'ich Y koordinatasini olish
+    };
+
+    const handleTouchMove = (e) => {
+        setTouchEnd(e.touches[0].clientY); // Harakat davomida Y koordinatasini olish
+    };
+
+    const handleTouchEnd = () => {
+        if (touchEnd - touchStart > 100) { // Agar 100px yoki undan ko'p pastga harakat bo'lsa
+            closeModal();
         }
     };
 
     return (
         <section className="shop_page">
-            {/* Banner bo'limi */}
-            <div className="shop_banner container">
-                <Swiper
-                    className="product_slider"
-                    grabCursor={true}
-                    spaceBetween={20}
-                    slidesPerView={1.1}
-                >
-                    <SwiperSlide>
-                        <img src={Banner} alt="Banner 1" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={Banner2} alt="Banner 2" />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <img src={Banner3} alt="Banner 3" />
-                    </SwiperSlide>
-                </Swiper>
-
-                {/* Kategoriyalar uchun tugmalar */}
-                <Swiper
-                    className="btn-button"
-                    grabCursor={true}
-                    spaceBetween={20}
-                    slidesPerView={2.5}
-                >
-                    {category.map((cat) => (
-                        <SwiperSlide
-                            key={cat.id}
-                            onClick={() => scrollToCategory(cat.id - 1)}
-                        >
-                            <LocalGroceryStoreRoundedIcon />
-                            {cat.category_name}
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-
-            {/* Kategoriyalar */}
-            <div className="category_section container">
-                {category.map((cat, index) => (
+            {/* Product cards */}
+            <div className="product_row">
+                {[...Array(5)].map((_, index) => (
                     <div
-                        key={cat.id}
-                        className="category_block"
-                        ref={(el) => (categoryRefs.current[index] = el)} // Refni saqlash
+                        key={index}
+                        className="shop_product_card"
+                        onClick={() =>
+                            openModal({
+                                img_url: "https://via.placeholder.com/150",
+                                name: "Product " + (index + 1),
+                                info: "This is a sample product description.",
+                                price: "50000",
+                            })
+                        }
                     >
-                        <h2 className="category_title">
-                            <LocalGroceryStoreRoundedIcon />
-                            {cat.category_name}
-                        </h2>
-                        <div className="product_row">
-                            {products
-                                .filter(
-                                    (product) =>
-                                        product.category_id === cat.id.toString()
-                                )
-                                .map((product) => (
-                                    <div key={product.id} className="shop_product_card">
-                                        <img src={product?.img_url} alt=""/>
-                                        <div className="shop_product_text">
-                                            <h3>{product.name}</h3>
-                                            <p className="product_count">500 gr</p>
-                                            <p className="product_price">
-                                                {product.price} so'm
-                                            </p>
-                                        </div>
-
-                                    </div>
-                                ))}
-                        </div>
+                        <img src="https://via.placeholder.com/150" alt="Product" />
+                        <p>Product {index + 1}</p>
                     </div>
                 ))}
             </div>
+
+            {/* Modal */}
+            {modalOpen && (
+                <div
+                    className="shop_modal"
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                    onTouchEnd={handleTouchEnd}
+                >
+                    <div className="modal_content" ref={modalRef}>
+                        {selectedProduct && (
+                            <>
+                                <img
+                                    src={selectedProduct.img_url}
+                                    alt={selectedProduct.name}
+                                />
+                                <h3>{selectedProduct.name}</h3>
+                                <p>{selectedProduct.info}</p>
+                                <p>Price: {selectedProduct.price} so'm</p>
+                            </>
+                        )}
+                        <button onClick={closeModal}>Close</button>
+                    </div>
+                </div>
+            )}
         </section>
     );
 };
