8625ba5266169dd67f18c6cdb4dc405af71a97e0
ibrohim74 <xibroxim@gmail.com>
2024-11-21 23:36:07 +0500

    firsts

--- src/component/bottom/Bottom.jsx
+++ src/component/bottom/Bottom.jsx
@@ -7,50 +7,53 @@ import ArticleIcon from '@mui/icons-material/Article';
 import PersonIcon from '@mui/icons-material/Person';
 import foto from '../../assets/img/Ellipse 2.svg'
 import {useTranslation} from "react-i18next";
-import {Link, useParams} from "react-router-dom";
+import {Link, useLocation, useParams} from "react-router-dom";
 import {USER_HOME} from "../../utils/const.jsx";
 
 
 export default function Bottom() {
-  const {t} = useTranslation();
-const {user_id , language} = useParams()
-  return (
-    <section className='bottom'>
-            <div className="box-top" >
-              <img src={foto} alt="" />
-              <div className="icon">
-                <ShoppingBasketIcon />
-              </div>
-              <p className="last">83 856 UZS</p>
-              <p className="last_savat">{t("bottomBar.basket")}</p>
+    const {t} = useTranslation();
+    const {user_id, language} = useParams()
+    const location = useLocation();
+    return (
+        <section className='bottom'>
+            <div className="box-top">
+                <img src={foto} alt=""/>
+                <div className="icon">
+                    <ShoppingBasketIcon/>
+                </div>
+                <p className="last">83 856 UZS</p>
+                <p className="last_savat">{t("bottomBar.basket")}</p>
             </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="page">
-              <Link to={USER_HOME.replace(":user_id", user_id).replace(":language", language)} className="box">
-                <HomeIcon />
-                <p className="bottom">{t("bottomBar.home")}</p>
-              </Link>
-              <div className="box" style={{marginLeft:"14px"}}>
-                <SearchIcon />
-                <p className="bottom">{t("bottomBar.search")}</p>
-              </div>
-              <div className="box" style={{marginRight:"100px"}}>
-                <p className="bottom"></p>
-              </div>
-              <div className="box" >
-                <ArticleIcon />
-                <p className="bottom">{t("bottomBar.news")}</p>
-              </div>
-              <div className="box">
-                <PersonIcon />
-                <p className="bottom">{t("bottomBar.profile")}</p>
-              </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="page">
+                            <Link to={USER_HOME.replace(":user_id", user_id).replace(":language", language)}
+                                  className={`box ${location.pathname === `/${user_id}/${language}` ? 'active' : ''}`}>
+                                <HomeIcon/>
+                                <p className="bottom">{t("bottomBar.home")}</p>
+                            </Link>
+                            <div className={`box ${location.pathname === `/${user_id}/${language}/search` ? 'active' : ''}`}
+                                 style={{marginLeft: "14px"}}>
+                                <SearchIcon/>
+                                <p className="bottom">{t("bottomBar.search")}</p>
+                            </div>
+                            <div className="box" style={{marginRight: "100px"}}>
+                                <p className="bottom"></p>
+                            </div>
+                            <div  className={`box ${location.pathname === `/${user_id}/${language}/news` ? 'active' : ''}`}>
+                                <ArticleIcon/>
+                                <p className="bottom">{t("bottomBar.news")}</p>
+                            </div>
+                            <div  className={`box ${location.pathname === `/${user_id}/${language}/profile` ? 'active' : ''}`}>
+                                <PersonIcon/>
+                                <p className="bottom">{t("bottomBar.profile")}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
