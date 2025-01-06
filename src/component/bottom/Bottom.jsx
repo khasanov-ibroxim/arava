89de2bbd610901d91fd6ae4ef8693fc07df2afc5
khasanov-ibroxim <xibroxim11@gmail.com>
2025-01-07 03:25:12 +0500

    update

--- src/component/bottom/Bottom.jsx
+++ src/component/bottom/Bottom.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Bottom.css"
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ArticleIcon from '@mui/icons-material/Article';
 import PersonIcon from '@mui/icons-material/Person';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import {useTranslation} from "react-i18next";
 import {Link, useLocation, useParams} from "react-router-dom";
-import {USER_BASKET_BAR, USER_HOME, USER_PROFILE} from "../../utils/const.jsx";
+import {USER_BASKET_BAR, USER_HOME, USER_NEWS, USER_PROFILE, USER_SEARCH} from "../../utils/const.jsx";
 
 
 export default function Bottom() {
@@ -14,34 +16,29 @@ export default function Bottom() {
     const location = useLocation();
     return (<>
         <footer>
+            <Link
+                to={USER_HOME.replace(":user_id", user_id).replace(":language", language)}
+                className={`footer_item ${location.pathname === `/${user_id}/${language}` ? 'active' : ''}`}
+            >
+                <HomeIcon/>
+                {/*<p>Marketplace</p>*/}
+            </Link>
+            <Link
 
-            <div className="menu">
-                <Link to={USER_HOME.replace(":user_id", user_id).replace(":language", language)} className="link">
-                   <span className={`link-icon ${location.pathname === `/${user_id}/${language}` ? 'active' : ''}`}>
-                       <HomeIcon/>
-                   </span>
-                    <span className="link-title">Bosh sahifa</span>
-                </Link>
+                to={USER_BASKET_BAR.replace(":user_id", user_id).replace(":language", language)}
+                className={`footer_item ${location.pathname === `/${user_id}/${language}/user_basket_bar` ? 'active' : ''}`}
+            >
+                <ShoppingBasketIcon/>
+                {/*<p>Basket</p>*/}
+            </Link>
+            <Link
 
-
-                <Link className="link" to={USER_BASKET_BAR.replace(":user_id", user_id).replace(":language", language)}>
-                   <span
-                       className={`link-icon ${location.pathname === `/${user_id}/${language}/user_basket_bar` ? 'active' : ''}`}>
-                       <ShoppingCartIcon/>
-                   </span>
-                    <span className="link-title">Arava</span>
-                </Link>
-
-
-                <Link to={USER_PROFILE.replace(":user_id", user_id).replace(":language", language)} className="link">
-                    <span
-                        className={`link-icon ${location.pathname === `/${user_id}/${language}/user_profile` ? 'active' : ''}`}>
-                       <PersonIcon/>
-                    </span>
-                    <span className="link-title">Profile</span>
-                </Link>
-            </div>
-
+                to={USER_PROFILE.replace(":user_id", user_id).replace(":language", language)}
+                className={`footer_item ${location.pathname === `/${user_id}/${language}/user_profile` ? 'active' : ''}`}
+            >
+                <PersonIcon/>
+                {/*<p>Profile</p>*/}
+            </Link>
         </footer>
     </>)
 }
